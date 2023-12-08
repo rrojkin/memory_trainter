5104bdeb563612581c0d82bbc01b0dfd58638655
Anton Razhnou <97039885+rrojkin@users.noreply.github.com>
2023-12-09 00:40:37 +0300

    WordsClassReworked

--- MemoryTrainer/WordsClass.swift
+++ MemoryTrainer/WordsClass.swift
@@ -13,95 +13,80 @@ class WordsClass {
         case sixLetters
         case sevenLetters
         case eightLettes
+        case any
     }
+        
+    let fiveLetterWords = ["домой", "ночьи", "книга", "мышка", "солью",
+                           "лампа", "рекаи", "крыша", "косой", "мочаа",
+                           "жить", "мытьь", "волна", "крыло", "мышьь"]
     
-    enum Level {
-        case one
-        case two
-        case three
-        case four
-    }
+    let sixLetterWords = ["кофеин", "рекаи", "морож", "краска", "песняа",
+                          "домико", "цветок", "ночкаа", "мышкаа", "снегок",
+                          "горкаа", "крышка", "домаш", "мышкаи", "крышаа"]
     
-    enum WordCount {
-        case five
-        case six
-        case seven
-        case eight
-    }
+    let sevenLetterWords = ["каранда", "слонико", "бревноо", "блинчи", "ребренк",
+                            "виногра", "грибоки", "космосы", "крепост", "цепочк",
+                            "рыбаков", "ложками", "человек", "пальтоо", "котлето"]
     
-    func getAmountOfRows(wordCount: WordCount) -> Int {
-        switch wordCount {
-        case .five:
-            return 5
-        case .six:
-            return 6
-        case .seven:
-            return 7
-        case .eight:
-            return 8
-        }
-    }
-    
-    private var lastRandomWord: String?
-    private var repeatedWords = [String]()
-    
-    let fiveLetterWords = ["a", "b", "c"]
-    let sixLetterWords = ["Сустав"]
-    let sevenLetterWords = ["Машинка"]
-    let eightLetterWords = ["Аванпост"]
+    let eightLetterWords = ["журналы", "случайно", "зонтиком", "домашний", "отражен",
+                            "деревце", "смешноо", "закатом", "шарфики", "бродяга",
+                            "пляжный", "печенье", "берегом", "лужайка", "заборчи"]
     
     var tempFiveLetterWords = [String]()
     var tempSixLetterWords = [String]()
     var tempSevenLetterWords = [String]()
     var tempEightLetterWords = [String]()
- 
-    func getRandomWord(_ level: Level,_ words: Words, _ wordCount: WordCount?) -> [String]? {
-        switch level {
-        case .one:
-            switch words {
-            case .fiveLetters:
-                return [tempFiveLetterWords.getRandomWord(fiveLetterWords)]
-                
-            case .sixLetters:
-                return [tempSixLetterWords.getRandomWord(sixLetterWords)]
-                
-            case .sevenLetters:
-                return [tempSevenLetterWords.getRandomWord(sevenLetterWords)]
-                
-            case .eightLettes:
-                return [tempEightLetterWords.getRandomWord(eightLetterWords)]
+    
+    func getRandomWord(_ words: Words, _ wordCount: Int) -> [String]? {
+        var wordArray = [String]()
+        
+        switch words {
+        case .fiveLetters:
+            for _ in 1...wordCount {
+                wordArray.append(tempFiveLetterWords.getRandomWord(fiveLetterWords))
+            }
+        case .sixLetters:
+            for _ in 1...wordCount {
+                wordArray.append(tempSixLetterWords.getRandomWord(sixLetterWords))
+            }
+        case .sevenLetters:
+            for _ in 1...wordCount {
+                wordArray.append(tempSevenLetterWords.getRandomWord(sevenLetterWords))
+            }
+        case .eightLettes:
+            for _ in 1...wordCount {
+                wordArray.append(tempEightLetterWords.getRandomWord(eightLetterWords))
             }
             
-        case .two, .three, .four:
-            var wordArray = [String]()
-            
-            guard let wordCount = wordCount else { return [String]() }
-            for _ in 1...getAmountOfRows(wordCount: wordCount) {
-                switch Words.allCases.randomElement()! {
-                case .fiveLetters:
-                    wordArray.append(fiveLetterWords.randomElement()!)
-                case .sixLetters:
-                    wordArray.append(fiveLetterWords.randomElement()!)
-                case .sevenLetters:
-                    wordArray.append(fiveLetterWords.randomElement()!)
-                case .eightLettes:
-                    wordArray.append(fiveLetterWords.randomElement()!)
+        case .any:
+            for _ in 1...wordCount {
+                var elem = Int.random(in: 1...4)
+                switch elem {
+                case 1:
+                    wordArray.append(tempFiveLetterWords.getRandomWord(fiveLetterWords))
+                case 2:
+                    wordArray.append(tempSixLetterWords.getRandomWord(sixLetterWords))
+                case 3:
+                    wordArray.append(tempSevenLetterWords.getRandomWord(sevenLetterWords))
+                case 4:
+                    wordArray.append(tempEightLetterWords.getRandomWord(eightLetterWords))
+                default:
+                    print("default")
+                }
+            }
+        }
+        return wordArray
+    }
+        
+        func checkForRepeatedWord(_ words: [String],_ word: String) -> Bool {
+            for w in words {
+                if w.capitalized == word.capitalized {
+                    return true
                 }
             }
             
-            return wordArray
+            return false
         }
-    }
-    
-    func checkForRepeatedWord(_ words: [String],_ word: String) -> Bool {
-        for w in words {
-            if w.capitalized == word.capitalized {
-                return true
-            }
-        }
-        
-        return false
-    }
 }
 
 extension Array<String> {
